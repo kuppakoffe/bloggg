c0341b15eb0f870a64ac0ce114f8dc1bbc02dd1c
kuppakoffe <sumit@logiq.ai>
2020-11-04 18:59:10 +0530

    Initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,9 +15,9 @@ function Index() {
   return (
     <main>
       <Helmet>
-        <title>Gatsby + Node.js (TypeScript) API</title>
+        <title>Sumit</title>
       </Helmet>
-      <h1>Gatsby + Node.js (TypeScript) API</h1>
+      <h1>Sumit</h1>
       <h2>
         Deployed with{' '}
         <a
@@ -30,21 +30,7 @@ function Index() {
         !
       </h2>
       <p>
-        <a
-          href="https://github.com/vercel/vercel/tree/master/examples/gatsby"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        is a <a href="https://www.gatsbyjs.org/">Gatsby</a> app with two
-        directories, <code>/src</code> for static content and <code>/api</code>{' '}
-        which contains a serverless{' '}
-        <a href="https://nodejs.org/en/">Node.js (TypeScript)</a> function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Node.js (TypeScript)
-        </a>
-        .
+        Site under construction.
       </p>
       <br />
       <h2>The date according to Node.js (TypeScript) is:</h2>
